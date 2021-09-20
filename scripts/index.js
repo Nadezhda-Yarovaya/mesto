3478c9ad883af67bcc36a4359e3194c5d615cfb1
Nadezhda Yarovaya <seriozz@yandex.ru>
2021-09-20 16:23:58 +0300

    2009 after 1st iteration

--- scripts/index.js
+++ scripts/index.js
@@ -1,15 +1,30 @@
 /*объявляю переменные  */
-const popup = document.querySelector(".popup");
-const openChngBtn =  document.querySelector(".profile__edit-btn"); 
-const closePopupBtn = document.querySelector(".popup__close-btn"); 
-const formElement = document.querySelector(".popup__form");
-const like = document.querySelectorAll(".elements__like");
-
-
+const popup = document.querySelector('.popup');
+const openFormButton = document.querySelector('.profile__edit-btn');
+const closePopupButton = document.querySelector('.popup__close-btn');
+const popupForm = document.querySelector('.popup__form');
+const like = document.querySelectorAll('.elements__like');
+let nameInput = document.querySelector('.profile__name');
+let jobInput = document.querySelector('.profile__job');
+let nameResult = popup.querySelector('.popup__input_type_name');
+let jobResult = popup.querySelector('.popup__input_type_job');
 
 /*прописываю функции*/
+function popupOpen() {
+  nameResult.value = nameInput.textContent;
+  jobResult.value = jobInput.textContent;
+  formToggle();
+}
+
+function popupClose(event) {
+  event.preventDefault();
+  nameInput.textContent = nameResult.value;
+  jobInput.textContent = jobResult.value;
+  formToggle();
+}
+
 function formToggle() {
-  popup.classList.toggle("popup_opened");
+  popup.classList.toggle('popup_opened');
 }
 
 function clickOutsd(event) {
@@ -18,29 +33,18 @@ function clickOutsd(event) {
   }
 }
 
-function formSubmitHandler(event) {
-  event.preventDefault();
-  let nameInput = document.querySelector(".profile__name"); 
-  let jobInput = document.querySelector(".profile__job");
-  let nameResult = popup.querySelector(".popup__name");
-  let jobResult = popup.querySelector(".popup__job");
-  nameInput.textContent = nameResult.value;
-  jobInput.textContent = jobResult.value;
-  formToggle();
-}
-
 function likeToggle(event) {
-  let likeActv = event.currentTarget;
-  likeActv.classList.toggle("elements__like_active");
+  let likeActive = event.currentTarget;
+  likeActive.classList.toggle('elements__like_active');
 }
 
 /*вешаю все функции на события теперь */
-openChngBtn.addEventListener("click", formToggle);
-closePopupBtn.addEventListener("click", formToggle);
-popup.addEventListener("click", clickOutsd);
-formElement.addEventListener("submit", formSubmitHandler);
+openFormButton.addEventListener('click', popupOpen);
+closePopupButton.addEventListener('click', popupClose);
+popupForm.addEventListener('submit', popupClose);
+
+popup.addEventListener('click', clickOutsd);
 
 for (let i = 0; i < like.length; i++) {
-  like[i].addEventListener("click", likeToggle);
+  like[i].addEventListener('click', likeToggle);
 }
-
